f622ead2868b0e5286aba01814f8d72ba4e25493
wnxnzhao <wenxinzh44@gmail.com>
2025-08-18 16:26:52 +0800

    file deletions

---
+++ pages/4_Methodology.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+# region <--------- Streamlit App Configuration --------->
+st.set_page_config(
+    layout="centered",
+    page_title="Energy Saver Advisor"
+)
+# endregion <--------- Streamlit App Configuration --------->
+
+st.title("Methodology")
+
+st.header("WattSaver: Your Friendly Energy Advisor")
+
+st.header("Climate Vouchers Eligibility Checker")
